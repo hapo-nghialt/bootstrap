29f2952a1ed025f79388b74f0dfc0948affea9b7
hapo-nghialt <nghia.lt@haposoft.com>
2020-07-30 10:51:28 +0700

    add header

---
+++ js/myjs.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.btn-header').click(function() {
+        $('.btn-icon').toggleClass('fa-times');
+        $('.btn-icon').toggleClass('fa-bars');
+    });
+});
